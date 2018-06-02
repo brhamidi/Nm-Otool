e0e09c121df8fa11c43b762d9429081839e488e0
Brahim HAMIDI <bhamidi@e2r1p2.42.fr>
2018-06-02 15:03:44 +0200

    wip

--- includes/ft_nm.h
+++ includes/ft_nm.h
@@ -6,7 +6,7 @@
 /*   By: bhamidi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/20 16:31:03 by bhamidi           #+#    #+#             */
-/*   Updated: 2018/06/01 18:24:14 by bhamidi          ###   ########.fr       */
+/*   Updated: 2018/06/02 14:43:08 by bhamidi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@
 # include <sys/mman.h>
 # include <stdio.h>
 # include <mach-o/loader.h>
+# include <mach-o/nlist.h>
 
 # include "libft.h"
 
--- srcs/handle_64.c
+++ srcs/handle_64.c
@@ -6,13 +6,24 @@
 /*   By: bhamidi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/01 17:55:13 by bhamidi           #+#    #+#             */
-/*   Updated: 2018/06/01 18:56:06 by bhamidi          ###   ########.fr       */
+/*   Updated: 2018/06/02 15:00:59 by bhamidi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_nm.h"
 
-int		load_cmd(const struct load_command *lc, const size_t file_size,
+int	print_sym(void *ptr, const struct symtab_command *symtab, const unsigned int acc)
+{
+	const struct nlist_64	*nlist = ptr + symtab->symoff;
+	const char				*string_table = ptr + symtab->stroff;
+
+	if (acc == symtab->nsyms)
+		return (0);
+	ft_putendl(string_table + nlist[acc].n_un.n_strx);
+	return (print_sym(ptr, symtab, acc + 1));
+}
+
+int	load_cmd(void *ptr, const struct load_command *lc, const size_t size,
 		const unsigned int acc, const uint32_t ncmds)
 {
 	if (acc == ncmds)
@@ -21,21 +32,15 @@ int		load_cmd(const struct load_command *lc, const size_t file_size,
 		return (1);
 	}
 	if (lc->cmd == LC_SYMTAB)
-	{
-		ft_putendl("match");
-		return (0);
-	}
-	return (load_cmd((void *)lc + lc->cmdsize, file_size, acc + 1, ncmds));
+		return (print_sym(ptr, (struct symtab_command *)lc, 1));
+	return (load_cmd(ptr, (void *)lc + lc->cmdsize, size, acc + 1, ncmds));
 }
 
-int		handle_64(void *ptr, const size_t file_size)
+int	handle_64(void *ptr, const size_t file_size)
 {
-	struct mach_header_64	*header;
+	const struct mach_header_64 * header = (struct mach_header_64 *)ptr;
 
 	if (sizeof(struct mach_header_64) > file_size)
 		return (1);
-	header = (struct mach_header_64 *)ptr;
-	if (sizeof(*header) + header->sizeofcmds > file_size)
-		return (1);
-	return (load_cmd(ptr + sizeof(*header), file_size, 0, header->ncmds));
+	return (load_cmd(ptr, ptr + sizeof(*header), file_size, 0, header->ncmds));
 }
