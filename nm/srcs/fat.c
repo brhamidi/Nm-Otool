b8efec3fb7f8ae4cd48bc0f4e313d0bbaf5a01d2
Brahim Hamidi <bhamidi@student.42.fr>
2018-06-28 13:26:09 +0200

    handle fat64

--- nm/srcs/fat.c
+++ nm/srcs/fat.c
@@ -6,7 +6,7 @@
 /*   By: bhamidi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/13 18:48:56 by bhamidi           #+#    #+#             */
-/*   Updated: 2018/06/27 17:36:35 by bhamidi          ###   ########.fr       */
+/*   Updated: 2018/06/28 13:25:43 by bhamidi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -103,6 +103,48 @@ int			obj_fat(t_info *inf)
 
 int		obj_fat64(t_info *inf)
 {
-	(void)inf;
-	return (43);
+	struct fat_header	*fheader;
+	struct fat_arch_64	*farch;
+	unsigned int		i;
+	t_info				new;
+
+	if (check(inf, inf->ptr, sizeof(*fheader) + sizeof(*farch)))
+		return (-1);
+	fheader = (struct fat_header *)inf->ptr;
+	farch = (struct fat_arch_64 *)(fheader + 1);
+	i = -1;
+	while (++i < rev(fheader->nfat_arch, 0, sizeof(uint32_t), inf->endian))
+	{
+		if (inf->mode == FULL && i != rev(fheader->nfat_arch,
+					0, sizeof(uint32_t), inf->endian))
+			ft_putchar('\n');
+		if (check(inf, farch + i, sizeof(*farch)))
+			return (-1);
+		if (rev(farch[i].cputype, 0,
+					sizeof(cpu_type_t), inf->endian) == CPU_TYPE_X86_64)
+			inf->mode = SINGLE;
+		new.ptr = inf->ptr +
+			rev(farch[i].offset, 0, sizeof(uint64_t), inf->endian);
+		new.end = new.ptr +
+			rev(farch[i].size, 0, sizeof(uint64_t), inf->endian);
+		new.file_name = inf->file_name;
+		if (check(inf, new.ptr, 0) || check(inf, new.end, 0))
+			return (-1);
+		if (inf->mode == SINGLE && rev(farch[i].cputype, 0,
+					sizeof(cpu_type_t), inf->endian) == CPU_TYPE_X86_64)
+			return (analyse_object(&new));
+		if (inf->mode == FULL)
+		{
+			ft_putstr(inf->file_name);
+			ft_putstr(" (for architecture ");
+			ft_putstr(get_cputype(rev(farch[i].cputype,
+							0, sizeof(cpu_type_t), inf->endian)));
+			ft_putendl("):");
+			analyse_object(&new);
+		}
+	}
+	if (inf->mode == FULL)
+		return (0);
+	inf->mode = FULL;
+	return (obj_fat64(inf));
 }
